eae4df04cf1fb9fd05f817a976de113c080467b9
Will Mason <willchido@gmail.com>
2013-02-06 17:45:42 -0600

    First draft of doxygen support

--- include/chucho/status.hpp
+++ include/chucho/status.hpp
@@ -10,31 +10,105 @@
 namespace chucho
 {
 
+/** 
+ * @class status status.hpp chucho/status.hpp 
+ *
+ * Chucho reports what is happening internally using status 
+ * messages. These can be monitored by querying the 
+ * status_manager or by installing a custom status_observer. 
+ */
 class CHUCHO_EXPORT status
 {
 public:
+    /**
+     * The type of clock used for time representation.
+     */
     typedef std::chrono::system_clock clock_type;
+    /**
+     * The type of time point.
+     */
     typedef std::chrono::time_point<clock_type> time_type;
 
+    /**
+     * The level of this status event.
+     */
     enum class level
     {
+        /** 
+         * Information only. Nothing is wrong. 
+         */ 
         INFO,
+        /** 
+         * Something went wrong, but not badly enough to inhibit
+         * functionality. 
+         */
         WARNING,
+        /** 
+         * Chucho is prevented from doing something that it should be 
+         * doing.
+         */
         ERROR
     };
 
+    /**
+     * @name Constructor
+     */
+//@{
+    /**
+     * Construct a status event.
+     * 
+     * @param lvl the level
+     * @param message the status message
+     * @param origin the status origin, usually the name of the 
+     *               class that is responsible for this status
+     * @param ex any associated exception
+     */
     status(level lvl,
            const std::string& message,
            const std::string& origin,
            std::exception_ptr ex = std::exception_ptr());
+//@}
 
+    /**
+     * Return the exception associated with this status.
+     * 
+     * @return the exception
+     */
     std::exception_ptr get_exception() const;
+    /**
+     * Return the level of this status.
+     * 
+     * @return the level
+     */
     level get_level() const;
+    /**
+     * Return the message of this status.
+     * 
+     * @return the message
+     */
     const std::string& get_message() const;
+    /**
+     * Return the origin of this status. The origin is usually the 
+     * class name of the object that created the status. 
+     * 
+     * @return the origin
+     */
     const std::string& get_origin() const;
+    /**
+     * Return the time of this status. The time is the time at which 
+     * the status was created. 
+     * 
+     * @return the time
+     */
     const time_type& get_time() const;
 
 protected:
+    /**
+     * Send a status to an output stream.
+     *  
+     * @param stream the output stream 
+     * @param st the status object 
+     */
     friend CHUCHO_EXPORT std::ostream& operator<< (std::ostream& stream, const status& st);
 
 private:
