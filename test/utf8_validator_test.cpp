1fae6b8dd57eb13d18f626d291d7a36f20a7a7ac
Will Mason <willchido@gmail.com>
2013-03-06 19:46:24 -0600

    Fixed #51. We now have our own UTF-8 validator.

---
+++ test/utf8_validator_test.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+#include <chucho/utf8_validator.hpp>
+#include <chucho/exception.hpp>
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+TEST(utf8_validator, invalid)
+{
+    std::vector<std::string> v =
+    {
+        "\x80",
+        "\xf5",
+        "\xc1\x7f",
+        "\xe9\x80\xff",
+        "\xf1\xa9\xc0\x80"
+    };
+    for (const std::string& s : v)
+    {
+        std::cout << "Check " << s << std::endl;
+        std::istringstream stream(s);
+        EXPECT_THROW(chucho::utf8::validate(stream), chucho::exception);
+    }
+}
+
+TEST(utf8_validator, valid)
+{
+    std::vector<std::string> v =
+    {
+        "I know you are, but what am I",
+        "\xc0\xb0, but what am I",
+        "I know you are, but \xdf\xa1",
+        "\xe0\x80\x81, but what am I",
+        "I know you are, but \xef\xbe\xbf",
+        "\xf0\x80\x81\x82, but what am I",
+        "I know you are, but \xf4\xbd\xbe\xbf"
+    };
+    for (const std::string& s : v)
+    {
+        std::cout << "Check " << s << std::endl;
+        std::istringstream stream(s);
+        EXPECT_NO_THROW(chucho::utf8::validate(stream));
+    }
+}
--- utf8_validator.cpp
+++ utf8_validator.cpp
@@ -16,8 +16,57 @@
 
 #include <chucho/utf8_validator.hpp>
 #include <chucho/exception.hpp>
-#include "utf8.h"
 #include <string>
+
+namespace
+{
+
+std::string::size_type find_invalid(const std::string& utf8)
+{
+    unsigned char ch;
+    std::string::size_type i = 0;
+    std::string::size_type j;
+    std::string::size_type end;
+    std::size_t trailing;
+    while (i < utf8.length())
+    {
+        ch = static_cast<unsigned char>(utf8[i]);
+        if (ch <= 0x7f)
+        {
+            i++;
+            continue;
+        }
+        else if (ch >= 0xc0 && ch <= 0xdf)
+        {
+            trailing = 1;
+        }
+        else if (ch >= 0xe0 && ch <= 0xef)
+        {
+            trailing = 2;
+        }
+        else if (ch >= 0xf0 && ch <= 0xf4)
+        {
+            trailing = 3;
+        }
+        else
+        {
+            return i;
+        }
+        end = i + trailing;
+        if (end >= utf8.length())
+            return i;
+        for (j = i + 1; j <= end; j++)
+        {
+            ch = static_cast<unsigned char>(utf8[j]);
+            if (ch < 0x80 || ch > 0xbf)
+                return i;
+        }
+        i = j;
+    }
+    return std::string::npos;
+}
+
+}
 
 namespace chucho
 {
@@ -32,11 +81,11 @@ void validate(std::istream& stream)
     while (std::getline(stream, line))
     {
         ++cur;
-        auto invalid = ::utf8::find_invalid(line.begin(), line.end());
-        if (invalid != line.end())
+        auto invalid = find_invalid(line);
+        if (invalid != std::string::npos)
         {
             throw exception("Found invalid UTF-8 at line " + std::to_string(cur) + ", column " +
-                std::to_string(std::distance(line.begin(), invalid)));
+                std::to_string(invalid + 1));
         }
     }
 }
