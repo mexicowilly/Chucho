e5120fc5fe295dd3fbe8992a47d50e4035451e5c
Will Mason <willchido@gmail.com>
2013-05-25 17:49:29 -0500

    Fixed #87. Yaml_formatter now uses YAML literal syntax in order to format messages with newlines.

--- yaml_formatter.cpp
+++ yaml_formatter.cpp
@@ -20,13 +20,40 @@
 #include <chucho/marker.hpp>
 #include <sstream>
 
+namespace
+{
+
+std::string to_yaml_literal(const std::string& msg, std::size_t indentation = 8)
+{
+    std::string result("|\n");
+    auto last_pos = msg.find_first_not_of(' ');
+    auto pos = msg.find('\n', last_pos);
+    while (pos != std::string::npos)
+    {
+        result.append(indentation, ' ');
+        result.append(msg, last_pos, pos - last_pos + 1);
+        last_pos = pos + 1;
+        pos = msg.find('\n', last_pos);
+    }
+    if (last_pos < msg.length())
+    {
+        result.append(indentation, ' ');
+        result.append(msg, last_pos, msg.length() - last_pos);
+    }
+    return result;
+}
+
+}
+
 namespace chucho
 {
 
 std::string yaml_formatter::format(const event& evt)
 {
+    static std::locale c_locale("C");
+
     std::ostringstream stream;
-    stream.imbue(std::locale("C"));
+    stream.imbue(c_locale);
     stream << "- event:\n";
     stream << "    version: 1\n";
     stream << "    milliseconds_since_epoch: " <<
@@ -42,7 +69,11 @@ std::string yaml_formatter::format(const event& evt)
         mstream << *evt.get_marker();
         stream << "    marker: " << utf8::escape_invalid(mstream.str()) << '\n';
     }
-    stream << "    message: " << utf8::escape_invalid(evt.get_message()) << '\n';
+    std::string msg = utf8::escape_invalid(evt.get_message());
+    std::size_t nl_count = std::count(msg.begin(), msg.end(), '\n');
+    if (nl_count > 1 || (nl_count == 1 && msg[msg.length() - 1] != '\n'))
+        msg = to_yaml_literal(msg);
+    stream << "    message: " << msg << '\n';
     return stream.str();
 }
 
