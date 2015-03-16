e18d60c4229e81e6dd76c1dd62ffab495949d131
Will Mason <willchido@gmail.com>
2015-03-16 10:10:36 -0600

    Fixed #232. Stopped trying to inline template methods

--- include/chucho/optional.hpp
+++ include/chucho/optional.hpp
@@ -175,31 +175,31 @@ optional<type>& optional<type>::operator= (const optional<type>& opt)
 }
 
 template <typename type>
-inline optional<type>::operator bool () const
+optional<type>::operator bool () const
 {
     return initialized_;
 }
 
 template <typename type>
-inline type& optional<type>::operator* ()
+type& optional<type>::operator* ()
 {
     return reinterpret_cast<type&>(data_);
 }
 
 template <typename type>
-inline const type& optional<type>::operator* () const
+const type& optional<type>::operator* () const
 {
     return reinterpret_cast<const type&>(data_);
 }
 
 template <typename type>
-inline type* optional<type>::operator-> ()
+type* optional<type>::operator-> ()
 {
     return reinterpret_cast<type*>(&data_);
 }
 
 template <typename type>
-inline const type* optional<type>::operator-> () const
+const type* optional<type>::operator-> () const
 {
     return reinterpret_cast<const type*>(&data_);
 }
