510dd0d97353f006ae3ce37cd44dd17af064e076
Will Mason <willchido@gmail.com>
2016-06-14 12:14:49 -0700

    Got validation for adding new loggers

--- config-tool/qt/logger_win_controller.cpp
+++ config-tool/qt/logger_win_controller.cpp
@@ -17,6 +17,58 @@
 #include "logger_win_controller.hpp"
 #include "logger_emittable.hpp"
 #include <chucho/log.hpp>
+#include <assert.h>
+#include <vector>
+#include <QStyledItemDelegate>
+#include <QLineEdit>
+
+namespace
+{
+
+class logger_validator : public QValidator
+{
+public:
+    logger_validator(const std::vector<QString>& existing)
+        : existing_(existing)
+    {
+    }
+
+    virtual State validate(QString& input, int& pos) const override
+    {
+        State st = std::find(existing_.begin(), existing_.end(), input) == existing_.end() ?
+            Acceptable : Intermediate;
+        return st;
+    }
+
+private:
+    std::vector<QString> existing_;
+};
+
+class logger_delegate : public QStyledItemDelegate
+{
+public:
+    logger_delegate(QTreeWidget& logger_win)
+        : logger_win_(logger_win)
+    {
+    }
+
+    virtual QWidget* createEditor(QWidget* parent,
+                                  const QStyleOptionViewItem& option,
+                                  const QModelIndex& index) const override
+    {
+        QLineEdit* le = reinterpret_cast<QLineEdit*>(QStyledItemDelegate::createEditor(parent, option, index));
+        std::vector<QString> existing;
+        for (int i = 0; i < logger_win_.topLevelItemCount() - 1; i++)
+            existing.push_back(logger_win_.topLevelItem(i)->text(0));
+        le->setValidator(new logger_validator(existing));
+        return le;
+    }
+
+private:
+    QTreeWidget& logger_win_;
+};
+
+}
 
 namespace chucho
 {
@@ -27,16 +79,15 @@ namespace config
 logger_win_controller::logger_win_controller(Ui::main_ui& ui)
     : logger_win_(*ui.logger_win)
 {
-    //QObject::connect(ui.add_logger_action, SIGNAL(triggered()), this, SLOT(new_logger()));
     connect(ui.add_logger_action, SIGNAL(triggered()), SLOT(new_logger()));
     connect(&logger_win_, SIGNAL(itemDoubleClicked(QTreeWidgetItem*, int)), SLOT(item_double_clicked(QTreeWidgetItem*, int)));
     logger_win_.headerItem()->setText(1, "");
     logger_win_.setColumnWidth(0, 200);
+    logger_win_.setItemDelegateForColumn(0, new logger_delegate(logger_win_));
 }
 
 void logger_win_controller::item_double_clicked(QTreeWidgetItem* it, int col)
 {
-    CHUCHO_INFO_LGBL("topLevelItemCount() " << logger_win_.topLevelItemCount());
     if (logger_win_.indexOfTopLevelItem(it) == logger_win_.topLevelItemCount() - 1)
         new_logger();
 }
