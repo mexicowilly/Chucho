f0779a0f73a4edc7d6d63b8b6cfb3ffadf50fd2d
Will Mason <willchido@gmail.com>
2018-04-01 14:02:55 -0500

    Fix #80. Copyright includes 2018

--- include/chucho/interval_file_roll_trigger_memento.hpp
+++ include/chucho/interval_file_roll_trigger_memento.hpp
@@ -1,6 +1,6 @@
 
 /*
- * Copyright 2013-2017 Will Mason
+ * Copyright 2013-2018 Will Mason
  * 
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
