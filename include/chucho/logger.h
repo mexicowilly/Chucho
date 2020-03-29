1ae74270919e295022871548b01248d4a0317cc8
Will Mason <willchido@gmail.com>
2020-03-29 08:06:43 -0700

    Documented C API better

--- include/chucho/logger.h
+++ include/chucho/logger.h
@@ -26,6 +26,8 @@
 /**
  * @file
  * Chucho's logger functions
+ *
+ * @ingroup c_loggers
  */
 
 #if defined(__cplusplus)
@@ -33,6 +35,15 @@ extern "C"
 {
 #endif
 
+/**
+ * A logger. This type can be used with the functions @ref chucho_get_logger
+ * and @ref chucho_release_logger in order to make logging slightly more
+ * efficient, if and only if your compiler supports the _Generic keyword.
+ * In this case, you may use the logging macros with either a logger or
+ * the name of a logger as the lgr parameter. If your compiler does not
+ * support _Generic, then you may only use the name of the logger in the
+ * logging macros, so using this type will be nearly pointless.
+ */
 typedef struct chucho_logger_t chucho_logger_t;
 
 /**
@@ -48,13 +59,36 @@ typedef enum
   CHUCHO_FATAL  /**< Fatal */
 } chucho_level_t;
 
+/**
+ * Get a logger.
+ *
+ * @post The logger must be released with @ref chucho_release_logger
+ *
+ * @param[in] name the name of the logger
+ * @return the newly allocated logger
+ */
 CHUCHO_EXPORT chucho_logger_t* chucho_get_logger(const char* const name);
+/**
+ * Release a logger allocated by @ref chucho_get_logger
+ *
+ * @param[in] lgr the logger to release
+ */
 CHUCHO_EXPORT void chucho_release_logger(chucho_logger_t* lgr);
 
 /**
- * @ingroup c_miscellaneous
+ * Test whether a logger permits a given level.
+ *
+ * @param[in] lgr the logger to test
+ * @param[in] lvl the level
+ * @return non-zero if the logger permits the level, zero if it does not
  */
 CHUCHO_EXPORT int chucho_logger_permits(const chucho_logger_t* lgr, chucho_level_t lvl);
+/**
+ * Set a logger's level.
+ *
+ * @param[in] lgr the logger
+ * @param[in] lvl the level
+ */
 CHUCHO_EXPORT void chucho_logger_set_level(chucho_logger_t* lgr, chucho_level_t lvl);
 
 #if defined(__cplusplus)
